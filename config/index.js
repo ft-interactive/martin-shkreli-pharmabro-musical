5841f0b7ca3be59538b9451ed6b1254eda0625f5
Joanna Kao <joanna.kao@ft.com>
2017-05-12 18:26:49 -0400

    load text and annotations from spreadsheet

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
+import * as bertha from 'bertha-client';
 import article from './article';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
@@ -6,31 +7,27 @@ export default async () => {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  /*
-  An experimental demo that gets content from the API
-  and overwrites some model values. This requires the Link File
-  to have been published. Also next-es-interface.ft.com probably
-  isn't a reliable source. Also this has no way to prevent development
-  values being seen in productions... use with care.
 
-  try {
-    const a = (await axios(`https://next-es-interface.ft.com/content/${d.id}`)).data;
-    d.headline = a.title;
-    d.byline = a.byline;
-    d.summary = a.summaries[0];
-    d.title = d.title || a.title;
-    d.description = d.description || a.summaries[1] || a.summaries[0];
-    d.publishedDate = new Date(a.publishedDate);
-    f.comments = a.comments;
-  } catch (e) {
-    console.log('Error getting content from content API');
-  }
+  const data = await bertha.get('1ws2VYeSVaGn2ROwF_4zGEerqHFOXsgnTt1FHSKfbWWs', ['metadata|object', 'annotations', 'body|object']).then(a => a);
 
-  */
+  console.log(data);
 
   return {
     ...d,
     flags,
     onwardJourney,
+    data,
+    headline: data.metadata.headline || '',
+    summary: data.metadata.summary || '',
+    title: data.metadata.title || '',
+    description: data.metadata.description || '',
+    socialImage: data.metadata.socialImage || '',
+    socialHeadline: data.metadata.socialHeadline || '',
+    socialSummary: data.metadata.socialSummary || '',
+    twitterImage: data.metadata.twitterImage || '',
+    twitterText: data.metadata.twitterText || '',
+    twitterHeadline: data.metadata.twitterHeadline || '',
+    facebookImage: data.metadata.facebookImage || '',
+    facebookHeadline: data.metadata.facebookHeadline || '',
   };
 };

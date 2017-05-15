4fb26ba13ef28981a40ddc7586af3257fe9526f2
Joanna Kao <joanna.kao@ft.com>
2017-05-15 19:28:52 -0400

    change url

--- config/article.js
+++ config/article.js
@@ -4,8 +4,8 @@ export default () => ({ // eslint-disable-line
   id: '',
 
   // canonical URL of the published page
-  // https://ig.ft.com/martin-shkreli-pharma-bro-musical get filled in by the ./configure script
-  url: 'https://ig.ft.com/martin-shkreli-pharma-bro-musical',
+  // https://ig.ft.com/martin-shkreli-pharmabro-musical get filled in by the ./configure script
+  url: 'https://ig.ft.com/martin-shkreli-pharmabro-musical',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')

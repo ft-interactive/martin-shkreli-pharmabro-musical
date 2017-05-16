4b779c1a277602d8967762123abe797c0cf3ba28
Joanna Kao <joanna.kao@ft.com>
2017-05-16 17:49:27 -0400

    get rid of ES6 for now (build process problems)

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
-const audio = document.querySelector('audio');
+'use strict';
 
-audio.addEventListener('timeupdate', (a) => {
-  const percentProgress = (a.target.currentTime * 100) / a.target.duration;
+var audio = document.querySelector('audio');
 
-  const progressImage = document.querySelector('.g-audio--block img');
-  progressImage.style.left = `${Math.min(percentProgress, 97.5)}%`;
+audio.addEventListener('timeupdate', function (a) {
+  var percentProgress = a.target.currentTime * 100 / a.target.duration;
+
+  var progressImage = document.querySelector('.g-audio--block img');
+  progressImage.style.left = Math.min(percentProgress, 97.5) + '%';
 });
+
+// const audio = document.querySelector('audio');
+//
+// audio.addEventListener('timeupdate', (a) => {
+//   const percentProgress = (a.target.currentTime * 100) / a.target.duration;
+//
+//   const progressImage = document.querySelector('.g-audio--block img');
+//   progressImage.style.left = `${Math.min(percentProgress, 97.5)}%`;
+// });

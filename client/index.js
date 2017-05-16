c0234548731243d1f153dc0cb607da1aafb9ed5d
Joanna Kao <joanna.kao@ft.com>
2017-05-16 18:33:52 -0400

    fix left positioning so bobble head doesn't fall off right side of page

--- client/index.js
+++ client/index.js
@@ -5,8 +5,11 @@ var audio = document.querySelector('audio');
 audio.addEventListener('timeupdate', function (a) {
   var percentProgress = a.target.currentTime * 100 / a.target.duration;
 
+  var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var maxLeft = 100 * (1 - (35 / browserWidth));
+
   var progressImage = document.querySelector('.g-audio--block img');
-  progressImage.style.left = Math.min(percentProgress, 97.5) + '%';
+  progressImage.style.left = Math.min(percentProgress, maxLeft) + '%';
 });
 
 // const audio = document.querySelector('audio');

b5d5271e6caac8e3e282893ced25784fd2cd169f
Joanna Kao <joanna.kao@ft.com>
2017-05-17 10:56:32 -0400

    use es6

--- client/index.js
+++ client/index.js
@@ -1,22 +1,10 @@
-'use strict';
+const audio = document.querySelector('audio');
 
-var audio = document.querySelector('audio');
+audio.addEventListener('timeupdate', (a) => {
+  const percentProgress = (a.target.currentTime * 100) / a.target.duration;
+  const browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const maxLeft = 100 * (1 - (35 / browserWidth));
 
-audio.addEventListener('timeupdate', function (a) {
-  var percentProgress = a.target.currentTime * 100 / a.target.duration;
-
-  var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var maxLeft = 100 * (1 - (35 / browserWidth));
-
-  var progressImage = document.querySelector('.g-audio--block img');
-  progressImage.style.left = Math.min(percentProgress, maxLeft) + '%';
+  const progressImage = document.querySelector('.g-audio--block img');
+  progressImage.style.left = `${Math.min(percentProgress, maxLeft)}%`;
 });
-
-// const audio = document.querySelector('audio');
-//
-// audio.addEventListener('timeupdate', (a) => {
-//   const percentProgress = (a.target.currentTime * 100) / a.target.duration;
-//
-//   const progressImage = document.querySelector('.g-audio--block img');
-//   progressImage.style.left = `${Math.min(percentProgress, 97.5)}%`;
-// });
